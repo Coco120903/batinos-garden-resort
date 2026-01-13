69720eb1820ce3668e729b87161d23cf0c18ee60
Franco <francogarcia8899@gmail.com>
2026-01-14 07:00:40 +0800

    Improve MongoDB connection with better timeout settings and fix admin seed script

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,17 @@ async function connectDB(mongoUri) {
   if (!mongoUri) throw new Error("MONGODB_URI is required");
 
   mongoose.set("strictQuery", true);
-  await mongoose.connect(mongoUri);
+  
+  // Connection options for better reliability
+  const options = {
+    serverSelectionTimeoutMS: 30000, // 30 seconds
+    socketTimeoutMS: 45000, // 45 seconds
+    connectTimeoutMS: 30000, // 30 seconds
+    retryWrites: true,
+    w: 'majority',
+  };
+
+  await mongoose.connect(mongoUri, options);
 }
 
 module.exports = { connectDB };
--- backend/src/seed/createAdmin.js
+++ backend/src/seed/createAdmin.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const { connectDB } = require("../config/db");
+const { connectDB } = require("../config/db");
 
 async function createAdmin() {
   try {
